4cacbdd71796979d2bb7c7c6d0e956bc578df2d6
isaac2015212275 <isaac2015212275@gmail.com>
2017-03-03 21:24:43 +0800

    update

--- README.txt
+++ README.txt
@@ -1,16 +1,27 @@
 # Assignment1
 software_lab_1
 
-This assingment is based on the practice in the lab.
+This assignment is based on the practice in the lab.
 
 ================================================
 
 The request
-  record pokemon data --- done
-  type to lower case --- done
-  start game --- done
-  three kind of actions --- done
-  
+    Input and output: handle player's text command and dialog display ----- done
+    Use an ArrayList to store pokemon data, and use a HashMap to store what type of pokemon player has caught.
+
+    Random generated pokemon ----- done
+    Use Random to do this.
+
+    Calculate score correctly ----- done
+    Use a integer "score" to store score.
+
+    Count the number of pokemons for each type correctly ----- done
+    Use a HashMap to store types(key) and numbers(value). And after quit the game, use a MapEntry to go through all types.
+    I learn the data from http://blog.csdn.net/xueyepiaoling/article/details/5217709
+
+    Comments, coding style and documentation ----- done
+    comments already in my code.
 
 What feature I add?
-1.
+    1.A GUI like interface, easier and cleaner to see.
+    2.React to wrong action is random.
--- src/ss/labs/pokemon/Main.java
+++ src/ss/labs/pokemon/Main.java
@@ -1,7 +1,10 @@
 package ss.labs.pokemon;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.Iterator;
 
 public class Main {
 
@@ -34,6 +37,10 @@ public class Main {
             //TODO 1-2:scan pokemon type and save to a variable
             type = scanner.next();
 
+            //lower type
+            String typeTmp = type.toLowerCase();
+            type = typeTmp;
+
             System.out.println("Enter pokemon's move name:");
             //TODO 1-3:scan pokemon move's name
             move = scanner.next();
@@ -64,5 +71,106 @@ public class Main {
         }
         //TODO 6 print out how many type of pokemons in the pokedex
         System.out.println("number of types of pokemons: "+typeCountMap.size());
+
+        //initial Random
+        Random random = new Random();
+
+        //接下來幾行單純是要嗆輸錯指令的玩家
+        ArrayList<String> dirtyWords = new ArrayList();
+        Random randomDirtywords = new Random();
+        dirtyWords.add("Are you retard?");
+        dirtyWords.add("Are you idiot?");
+        dirtyWords.add("Are you db2?");
+        dirtyWords.add("Why type wrong words?");
+
+        //start the game GUI
+        System.out.println("==============================");
+        System.out.println("=                            =");
+        System.out.println("=         Game Start!        =");
+        System.out.println("=                            =");
+        System.out.println("==============================");
+
+        String command; //command
+        int choseMonster; //random number to chose pokemon
+        int score=0; //score
+        HashMap<String,Integer> catchedHash = new HashMap(); //hash for catched pokemon
+
+        do
+        {
+            choseMonster = random.nextInt(n); //pick a random number
+            PokemonData pokeDataTem = pokeDex.get(choseMonster); //pick pokemon
+
+            //顯示出現的怪 in fake GUI
+            System.out.println("==============================");
+            System.out.println("=   遇到了野生的"+pokeDataTem.name+"  =");
+            System.out.println("=     Type: "+pokeDataTem.type+"            =");
+            System.out.println("=     Move: "+pokeDataTem.move+"        =");
+            System.out.println("==============================");
+
+            //顯示可行的行動 in fake GUI
+            System.out.println("==============================");
+            System.out.println("=     catch         run      =");
+            System.out.println("=                            =");
+            System.out.println("=     quit                   =");
+            System.out.println("==============================");
+            System.out.println("enter your reaction: ");
+            command = scanner.next();
+            if(command.equals("catch"))
+            {
+                //fake GUI
+                System.out.println("==============================");
+                System.out.println("=   You  catch               =");
+                System.out.println("=              "+pokeDataTem.name+"!     =");
+                System.out.println("==============================");
+                //score system
+                if(pokeDataTem.type.equals("grass"))
+                    score++;
+                else
+                    score--;
+                //hash system
+                if(catchedHash.containsKey(pokeDataTem.type))
+                    catchedHash.put(pokeDataTem.type,catchedHash.get(pokeDataTem.type)+1);
+                else
+                    catchedHash.put(pokeDataTem.type,1);
+            }
+            else if(command.equals("run"))
+            {
+                //fake GUI
+                System.out.println("==============================");
+                System.out.println("=   You                      =");
+                System.out.println("=              run away!     =");
+                System.out.println("==============================");
+                //score system
+                if(pokeDataTem.type.equals("grass"))
+                    score--;
+                else
+                    score++;
+            }
+            else if(command.equals("quit"));
+            else
+            {
+                //fake GUI
+                System.out.println("==============================");
+                System.out.println("=       "+dirtyWords.get(randomDirtywords.nextInt(4))+"       =");
+                System.out.println("=     "+pokeDataTem.name+"   run Away!   =");
+                System.out.println("==============================");
+            }
+        }while(!command.equals("quit"));
+
+        //quit game
+        System.out.println("==============================");
+        System.out.println("=     You quit the game!     =");
+        System.out.println("=     Score: "+score+"              =");
+
+        //use iterator to go through every kind you catch
+        Iterator it = catchedHash.entrySet().iterator();
+        while(it.hasNext())
+        {
+            Map.Entry entry = (Map.Entry) it.next();
+            Object key = entry.getKey(); //get the type
+            Object val = entry.getValue(); //get the numer
+            System.out.println("=    "+key+": "+val+"         =");
+        }
+        System.out.println("==============================");
     }
 }
